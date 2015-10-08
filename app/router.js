b0f9e38ca8025aa0c89c0580d42c4f5a9ceb570d
Mike North <michael.l.north@gmail.com>
2015-10-07 22:19:02 -0500

    Exercise 6

--- app/router.js
+++ app/router.js
@@ -9,6 +9,7 @@ Router.map(function() {
   this.route('orgs');
   this.route('org', {path: 'org/:id'}, function() {
     this.route('repos');
+    this.route('notfound');
     this.route('repo', {path: ':idd'}, function() {
       this.route('issues');
       this.route('contributors');
--- app/routes/org.js
+++ app/routes/org.js
@@ -1,6 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+actions: {
+  error(jqXhr/*, transition, route*/) {
+    if (jqXhr.status === 404) {
+      this.intermediateTransitionTo('org.notfound');
+      return true;
+    } else {
+      return false; // Don't enter error substate
+    }
+  }
+},
   model(params) {
     return $.get(`https://api.github.com/orgs/${params.id}`).then(raw => {
       raw.oldId = raw.id;
